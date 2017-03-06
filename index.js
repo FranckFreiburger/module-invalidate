eb01c23cd9100d4de60870604e10bf90aaab868d
Franck Freiburger <franck.freiburger@gmail.com>
2017-03-06 16:36:55 +0100

    - typo

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function bindSetProto(fct, value) {
 		return fct.apply(value, arguments);
 	}
 	Object.setPrototypeOf(bound, fct); // see test "exports property on function"
-	delete bound.name; // preserves the bound function name
+	delete bound.name; // preserves the original function name
 	return bound;
 }
 
